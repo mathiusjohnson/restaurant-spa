eae1bd5668b432ff3db90957ee592c99f90e0a37
Mathius Johnson <devachievem@gmail.com>
2020-08-23 14:17:13 -0700

    seeds and queries started

---
+++ db/seeds/02_orders.sql
@@ -0,0 +1,9 @@
+INSERT INTO orders
+  (customer_id, order_date)
+VALUES
+  (1, 2020/09/22);
+INSERT INTO orders
+  (customer_id, order_date)
+VALUES
+  (2, 2020/09/22);
+
---
+++ db/seeds/03_order_items.sql
@@ -0,0 +1,20 @@
+INSERT INTO order_items
+  (menu_item_id, order_id, quantity)
+VALUES
+  (1, 1, 1);
+INSERT INTO order_items
+  (menu_item_id, order_id, quantity)
+VALUES
+  (3, 1, 1);
+INSERT INTO order_items
+  (menu_item_id, order_id, quantity)
+VALUES
+  (4, 1, 2);
+INSERT INTO order_items
+  (menu_item_id, order_id, quantity)
+VALUES
+  (6, 2, 1);
+INSERT INTO order_items
+  (menu_item_id, order_id, quantity)
+VALUES
+  (3, 2, 2);
