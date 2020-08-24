438a6ea85588dd783151fe37077adcfa60bf1a81
Mathius Johnson <devachievem@gmail.com>
2020-08-24 22:52:39 +0000

    twilio

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,29 +1,18 @@
-const accountSid = 'ACb293030513abf9a16262c01ff0ef494e';
-const authToken = 'd88a7629e933defbdf39d7812271fcc5';
+
+const accountSid = process.env.TWILIO_ACCOUNTSID;
+const authToken = process.env.TWILIO_AUTHTOKEN;
+const fromPhone = process.env.TWILIO_NUMBER;
+
 const client = require('twilio')(accountSid, authToken);
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express();
+const sendMessage = (messageData) => {
+  return client.messages.create({
+    body: `Hi ${messageData.body.userName},
+      Thanks for ordering with LightResto.
+      Your food is ready for pickup`,
+    from: fromPhone,
+    to: `${messageData.body.sendText}`
+  });
+};
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
-
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
-
-client.messages
-  .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs? Also hi Serah it\'s Mathius hahahahah',
-    from: '+14132254219',
-    to: '+12368388913'
-  })
-  .then(message => console.log(message.sid));
+module.exports = { sendMessage };
