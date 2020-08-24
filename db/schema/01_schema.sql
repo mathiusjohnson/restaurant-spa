819c111150462c8031c5ed0bc8d6605ebb563b47
Serah Sirohi <serahsirohi@gmail.com>
2020-08-24 02:54:51 +0000

    adding changes to schema

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -2,8 +2,9 @@
 
 DROP TABLE IF EXISTS customers CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS preservations CASCADE;
-DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS menu_items CASCADE;
 
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -14,14 +15,14 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES customers(id) NOT NULL,
   order_date DATE NOT NULL
-)
+);
 
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   menu_item_id INTEGER REFERENCES menu_items(id),
   order_id INTEGER REFERENCES orders(id),
   quanity INTEGER
-)
+);
 
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -31,7 +32,7 @@ CREATE TABLE categories (
   -- kids_menu VARCHAR(255) NOT NULL,
   -- desserts VARCHAR(255) NOT NULL,
   -- drinks VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -39,4 +40,4 @@ CREATE TABLE menu_items (
   price INTEGER NOT NULL DEFAULT 0,
   description TEXT,
   category_id INTEGER REFERENCES categories(id)
-)
+);
