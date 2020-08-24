1de3437bccce9b8b650580b0345289d0feb4f62c
Serah Sirohi <serahsirohi@gmail.com>
2020-08-24 17:49:56 +0000

    added menu function

--- routes/database.js
+++ routes/database.js
@@ -56,3 +56,17 @@ const pstCalc = (id) => {
         .then(res => res.rows);
 };
 exports.gstCalc = pstCalc;
+
+const menuItems = (id) => {
+
+    const queryString = (`SELECT menu_items.*,
+        categories.name
+    FROM menu_items
+    JOIN categories ON category_id = categories.id
+    WHERE category_id = $1`, [id]);
+
+    return db.query(queryString, queryParams)
+        .then(res => res.rows);
+
+};
+exports.menuItems = menuItems;
