aa12c154b6047d0be01f55c147c4b67bf3e34d23
Mathius Johnson <devachievem@gmail.com>
2020-08-24 04:45:42 +0000

    database near finished

--- db/queries/dbqueries.sql
+++ db/queries/dbqueries.sql
@@ -10,13 +10,13 @@ FROM menu_items
   JOIN order_items ON menu_item_id = menu_items.id
 WHERE order_id = 1;
 
--- for customer specific orders
-SELECT customers.name, menu_items.name, price
-FROM customers
-  JOIN orders ON customer_id = customers.id
-  JOIN order_items ON order_id = orders.id
-  JOIN menu_items ON menu_item_id = menu_items.id
-WHERE customers.id = 1;
+-- -- for customer specific orders
+-- SELECT customers.name, menu_items.name, price
+-- FROM customers
+--   JOIN orders ON customer_id = customers.id
+--   JOIN order_items ON order_id = orders.id
+--   JOIN menu_items ON menu_item_id = menu_items.id
+-- WHERE customers.id = 1;
 
 -- total price
 SELECT SUM(price)
@@ -25,22 +25,12 @@ FROM menu_items
 WHERE order_id = 1
 GROUP BY order_id;
 
--- subtotal
-SELECT SUM(price)
+-- GST
+SELECT ROUND(((SUM(price)*.05)/100), 2)
 FROM menu_items
   JOIN order_items ON menu_item_id = menu_items.id
 WHERE order_id = 1
 GROUP BY menu_items.name;
-
-
--- functions
-place ORDER
-INSERT INTO
-
-
--- for add to order button
-add items
-(name, price and quantity) to cart
 
 
 
@@ -51,8 +41,3 @@ SELECT menu_items.*, categories.name
 FROM menu_items
   JOIN categories ON category_id = categories.id
 WHERE category_id = 1;
-
-
-
-
-show menu
--- db/seeds/03_order_items.sql
+++ db/seeds/03_order_items.sql
@@ -1,20 +1,20 @@
 INSERT INTO order_items
-  (menu_item_id, order_id, quantity)
+  (menu_item_id, order_id, customer_id, quantity)
 VALUES
-  (22, 1, 1);
+  (1, 1, 1, 1);
 INSERT INTO order_items
-  (menu_item_id, order_id, quantity)
+  (menu_item_id, order_id, customer_id, quantity)
 VALUES
-  (23, 1, 1);
+  (3, 1, 1, 1);
 INSERT INTO order_items
-  (menu_item_id, order_id, quantity)
+  (menu_item_id, order_id, customer_id, quantity)
 VALUES
-  (24, 1, 2);
+  (4, 1, 1, 2);
 INSERT INTO order_items
-  (menu_item_id, order_id, quantity)
+  (menu_item_id, order_id, customer_id, quantity)
 VALUES
-  (25, 2, 1);
+  (6, 2, 2, 1);
 INSERT INTO order_items
-  (menu_item_id, order_id, quantity)
+  (menu_item_id, order_id, customer_id, quantity)
 VALUES
-  (26, 2, 2);
+  (3, 2, 2, 2);
